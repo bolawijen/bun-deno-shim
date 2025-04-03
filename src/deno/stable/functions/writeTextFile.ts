064c40250f150a6a07c84ed23571ddca1229d817
Yusup Hambali <sup.alpuket@gmail.com>
2025-04-04 01:08:28 +0700

    use bun native API

--- src/deno/stable/functions/writeTextFile.ts
+++ src/deno/stable/functions/writeTextFile.ts
@@ -1,8 +1,8 @@
 ///<reference path="../lib.deno.d.ts" />
 
-import * as fs from "fs/promises";
 import mapError from "../../internal/errorMap.js";
 import { getFsFlag } from "../../internal/fs_flags.js";
+import { write } from 'bun';
 
 export const writeTextFile: typeof Deno.writeTextFile =
   async function writeTextFile(
@@ -19,8 +19,8 @@ export const writeTextFile: typeof Deno.writeTextFile =
       write: true,
     });
     try {
-      await fs.writeFile(path, data, { flag, mode, signal });
-      if (mode !== undefined) await fs.chmod(path, mode);
+      await write(path, data, { mode })
+      // if (mode !== undefined) await fs.chmod(path, mode);
     } catch (error) {
       throw mapError(error);
     }
